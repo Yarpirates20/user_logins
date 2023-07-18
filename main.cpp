1eba2f04f31c16b13689f544cb6d14db6a76ac07
Robert Samoraj <rsamoraj11@gmail.com>
2023-07-18 12:02:08 -0500

    Compare username and password to hardcoded values

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+// Prototypes
+bool is_valid_credentials(string, string);
+
 
 int main()
 {
+	string username, password;
+
+	cout << "Enter username: ";
+	cin >> username;
+
+	cout << "Enter password: ";
+	cin >> password;
+
+	if (is_valid_credentials(username, password))
+	{
+		cout << "\u001b[2J";
+		cout << "\nMy deepest, darkest secret is that I'm a potato.\n";
+	}
+	else
+	{
+		cout << "\u001b[2J";
+		cout << "\nGet lost hacky-sack!\n";
+	}
+
+
 	return 0;
+}
+
+bool is_valid_credentials(string uname, string passwd)
+{
+	if (uname == "robert" && passwd == "password123")
+	{
+		return true;
+	}
+
+	return false;
 }
