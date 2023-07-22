87eaa5b3bcee5839c815c3cf99126766e4194298
Robert Samoraj <rsamoraj11@gmail.com>
2023-07-22 10:10:05 -0500

    Add methods to user to set new username/password

--- User.cpp
+++ User.cpp
@@ -1,11 +1,36 @@
 #include "User.h"
 #include <iostream>
 #include <string>
+#include "sha256.h"
 using namespace std;
 
 void User::set_username()
 {
-	//TODO: Open database and check user names to see if it is all ready taken
+	
 	string uname;
+	//TODO: Open database and check user names to see if it is all ready taken
+	cout << "Type in your new username: ";
+	getline(cin, uname);
 
+	username = uname;
+
+}
+
+std::string User::get_username()
+{
+	return username;
+}
+
+void User::input_password()
+{
+	string pwd;
+	cout << "Enter your new password: ";
+	getline(cin, pwd);
+
+	hashed_password_input = sha256(pwd);
+}
+
+std::string User::get_hash()
+{
+	return hashed_password_input;
 }
--- User.h
+++ User.h
@@ -1,16 +1,16 @@
 #pragma once
 #include <string>
+#include "DB.h"
 class User
 {
 private:
 	std::string username;
-	std::string passwordInput;
+	std::string hashed_password_input;
 
 public:
 	void set_username();
 	std::string get_username();
 	void input_password();
-	void hash_password(std::string);
 	std::string get_hash();
 	void add_to_table(std::string, std::string);
 
