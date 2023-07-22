01a103b391be6f3b5e54c1a255362cba5f7f55a7
Robert Samoraj <rsamoraj11@gmail.com>
2023-07-21 21:27:23 -0500

    TODO: open database and check usernames

--- User.cpp
+++ User.cpp
@@ -1,1 +1,11 @@
 #include "User.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void User::set_username()
+{
+	//TODO: Open database and check user names to see if it is all ready taken
+	string uname;
+
+}
